05f1b294ab6e0507c1faf37c5bc2ada4fe901600
RocioDelAlb4 <rocio.of.alba@gmail.com>
2024-08-02 08:08:07 -0400

    base de categorias y ahorros

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -104,7 +104,7 @@ export default function PersistentDrawerLeft() {
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" noWrap component="div">
-            Persistent drawer
+            Gestion de gastos
           </Typography>
         </Toolbar>
       </AppBar>
